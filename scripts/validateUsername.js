6665892b967c6be6c7e6f273f5a6ad7420fbb80c
Nastya Stepanenko <63448464+nastya-star@users.noreply.github.com>
2021-12-28 00:26:08 +0200

    updated validation

--- scripts/validateUsername.js
+++ scripts/validateUsername.js
@@ -1,5 +1,5 @@
 module.exports = function (username) {
-    const regExp = /^([a-zA-Z]|([a-zA-Z]+\.+\s+[a-zA-Z])|([a-zA-Z]+\s+[a-zA-Z]))+$/g;
+    const regExp = /^([a-zA-Z]|([a-zA-Z]+\.+\s+[a-zA-Z])|([a-zA-Z]+\s+[a-zA-Z])|([a-zA-Z]+\.+[a-zA-Z]))+$/g;
 
     return Boolean(username.match(regExp));
 }
